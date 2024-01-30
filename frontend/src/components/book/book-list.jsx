4122f36e80df58bf6991857f38174ef79167b756
Gautam Chowdhury <gautam.zero@gmail.com>
2024-01-30 21:30:18 +0600

    init frontend with create-react-app, removed default app.jsx and configured react-router

---
+++ frontend/src/components/book/book-list.jsx
@@ -0,0 +1,7 @@
+export default function BookList() {
+  return (
+    <div>
+      <h1>Book List</h1>
+    </div>
+  );
+}
---
+++ frontend/src/routes.js
@@ -0,0 +1,16 @@
+import BookList from "./components/book/book-list";
+import ErrorPage from "./components/common/error-page";
+
+const routes = [
+    {
+        path: "/",
+        element: <BookList />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/add-book",
+        element: <div>Add book!</div>,
+    },
+];
+
+export default routes; 
